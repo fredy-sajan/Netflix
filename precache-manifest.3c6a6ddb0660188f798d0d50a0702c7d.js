af84b594a6c84962f5fdf29510a92afc7a8561ab
Fredy Sajan <f60984157@gmail.com>
2023-02-23 19:50:25 +0530

    Updates

---
+++ precache-manifest.3c6a6ddb0660188f798d0d50a0702c7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f631f74381f46a6494e",
+    "url": "/Netflix/static/css/main.54677d7a.chunk.css"
+  },
+  {
+    "revision": "1f631f74381f46a6494e",
+    "url": "/Netflix/static/js/main.1f631f74.chunk.js"
+  },
+  {
+    "revision": "f9e3b2efe9a7adba0ee0",
+    "url": "/Netflix/static/js/1.f9e3b2ef.chunk.js"
+  },
+  {
+    "revision": "2d300fbb7525717308e2",
+    "url": "/Netflix/static/js/runtime~main.2d300fbb.js"
+  },
+  {
+    "revision": "9d559cb9c66aa579563178e1efa49447",
+    "url": "/Netflix/index.html"
+  }
+];
